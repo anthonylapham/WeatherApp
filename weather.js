1cf131e5da33497210dc05c57a205498750956f2
Anthony Lapham <lapham.2@wright.edu>
2017-04-11 06:54:41 -0400

    no lat or lon

--- weather.js
+++ weather.js
@@ -4,7 +4,7 @@ $(document).ready(function() {
   $.getJSON(apiURL, function(data) {
       console.log(data);
       document.getElementById('weather').innerHTML = data.current_observation.weather;
-      document.getElementById('geo').innerHTML = "latitude: " + data.current_observation.display_location.latitude + " longitude: " + data.current_observation.display_location.longitude;
+      //document.getElementById('geo').innerHTML = "latitude: " + data.current_observation.display_location.latitude + " longitude: " + data.current_observation.display_location.longitude;
       document.getElementById('temp').innerHTML = data.current_observation.temperature_string;
       document.getElementById('city').innerHTML = data.current_observation.display_location.city;
       document.getElementById('icon').src= data.current_observation.icon_url;
