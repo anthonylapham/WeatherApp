bc4791cf8342084aabfa20f68ef332fd5ec81069
Anthony Lapham <lapham.2@wright.edu>
2017-04-10 20:01:20 -0400

    new api

--- weather.js
+++ weather.js
@@ -1,12 +1,12 @@
 $(document).ready(function() {
-  var apiURL = "http://api.openweathermap.org/data/2.5/weather?q=Dayton&APPID=f22ac9816b3179a0e9790bf47358fa0c";
+  var apiURL = "http://api.wunderground.com/api/a7221d2ea4ee3996/conditions/q/autoip.json";
 
   $.getJSON(apiURL, function(data) {
       console.log(data);
-      document.getElementById('weather').innerHTML = data.weather[0].description;
-      document.getElementById('geo').innerHTML = "latitude: " + data.coord.lat + " longitude: " + data.coord.lon;
-      document.getElementById('temp').innerHTML = data.main.temp;
-      document.getElementById('city').innerHTML = data.name;
+      document.getElementById('weather').innerHTML = data.current_observation.weather;
+      document.getElementById('geo').innerHTML = "latitude: " + data.current_observation.display_location.latitude + " longitude: " + data.current_observation.display_location.longitude;
+      document.getElementById('temp').innerHTML = data.current_observation.temperature_string;
+      document.getElementById('city').innerHTML = data.current_observation.display_location.city;
       var kelvin = data.main.temp;
       var c = kelvin-273;
       console.log(c);
@@ -14,14 +14,4 @@ $(document).ready(function() {
       console.log(f);
   });
 
-  function getLocation() {
-    if (navigator.geolocation) {
-        navigator.geolocation.getCurrentPosition(function(location){
-        $("#geo").html("latitude: " + geo.coord.lat + " longitude: " + geo.coord.lon);
-        $("#weather").html(weather.weather.description);
-        $("#temp").html(weather.main.temp);
-        $("#city").html(weather.name);
-        })
-      }
-    }
 });
